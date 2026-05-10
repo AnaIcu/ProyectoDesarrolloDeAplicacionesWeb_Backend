b2ad142829150cc45fc5b0f7302cab8a4e1218ff
AnaIcu <25004137@galileo.edu>
2026-05-10 08:23:43 -0600

    semana4

--- routes/goals.js
+++ routes/goals.js
@@ -8,25 +8,33 @@ let goals = [
 ];
 
 router.get('/getGoals', (req, res) => {
-  res.json(goals);
+  res.status(200).json(goals);
 });
 
 router.post('/addGoal', (req, res) => {
   const { name, description, duedate } = req.body;
-  const newGoal = { 
+  if(name && description && duedate) {
+     const newGoal = { 
     id_: Math.floor(Math.random() * 1000) + 1, 
     name, 
     description, 
     duedate 
   };
   goals.push(newGoal);
-  res.json(newGoal);
+  res.status(200).json(newGoal);
+} else {
+  res.status(400).json({ error: 'Please provide all required fields' });
+}
 });
 
 router.delete('/removeGoal/:id', (req, res) => {
-  const goalId = parseInt(req.params.id);
-  goals = goals.filter(goal => goal.id_ !== goalId);
-  res.json({ message: `Goal with id ${goalId} deleted` });
+  if(req.params && req.params.id && !isNaN(req.params.id)) {
+    const goalId = parseInt(req.params.id);
+    goals = goals.filter(goal => goal.id_ !== goalId);
+    res.status(200).json({ message: `Goal with id ${goalId} deleted` });
+  } else {
+    res.status(400).json({ error: 'Invalid goal ID' });
+  }
 });
 
 module.exports = router;
